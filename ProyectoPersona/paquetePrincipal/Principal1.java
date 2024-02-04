2d65eda1eaac51cb338c09e9f42a6756e6940f24
LunethIV <149028919+LunethIV@users.noreply.github.com>
2024-02-04 21:41:49 +0100

    Add files via upload

--- ProyectoPersona/paquetePrincipal/Principal1.java
+++ ProyectoPersona/paquetePrincipal/Principal1.java
@@ -1,12 +1,32 @@
 
 package paquetePrincipal;
 
-import paquetePersona.Nif;
+import paquetePersona.*;
 
 public class Principal1 {
 
     public static void main(String[] args) {
-        Nif a = new Nif("75941838R");
-                              
+        
+        // a) Introduce el dni (sin letra) de una persona por teclado.
+        int dni = Introducir.entero("Introducir dni:");
+        
+        // b) Crea un Nif con ese valor.
+        Nif a = new Nif(dni);       
+        
+        // c) Mostrar ese mismo Nif.
+        System.out.println(a);          
+        
+        // d) Introduce un nif (cadena) por teclado.
+        String dni2 = Introducir.cadena("Introduce el DNI con letra:");
+        
+        // e) Crea un Nif con ese valor.
+        Nif b = new Nif(dni2);
+        
+        // f) Comprobar si los Nif c) y e) son iguales.
+        if(a.equals(b)){
+            System.out.println("Son iguales");
+        }else{
+            System.out.println("No son iguales");
+        }
     }   
 }
